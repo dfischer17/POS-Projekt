21fb1505ca96ff331193adaad1c8f0ecd016ee19
Tobias <tobias.hansbauer19@gmail.com>
2020-06-21 22:07:55 +0200

    Detail Fragment Layout fixed

--- app/src/main/java/com/example/cloudclient/DetailActivity.java
+++ app/src/main/java/com/example/cloudclient/DetailActivity.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.cloudclient.fragments.MainFragment;
@@ -25,6 +26,7 @@ public class DetailActivity extends AppCompatActivity {
         TextView sizeView = findViewById(R.id.frag_size);
         TextView mimeTypeView = findViewById(R.id.frag_mimeType);
         TextView createdTimeView = findViewById(R.id.frag_createdTime);
+        ImageView imageView = findViewById(R.id.imageView);
 
         // Werte setzen
         FileDetails fileDetails = (FileDetails) getIntent().getSerializableExtra("details");
@@ -34,7 +36,11 @@ public class DetailActivity extends AppCompatActivity {
         String size = String.valueOf(fileDetails.getSize());
         String mimeType = fileDetails.getMimeType();
 
-            // Created Date parsen
+        //Backbutton
+        getSupportActionBar().setTitle("Details");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Created Date parsen
             DateTimeFormatter rfc3339Parser = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
             LocalDateTime createdTime = LocalDateTime.parse(fileDetails.getCreatedDate().toStringRfc3339(), rfc3339Parser);
 
@@ -43,10 +49,24 @@ public class DetailActivity extends AppCompatActivity {
 
         String createdDateString = createdTime.format(userDatePrinter);
 
+
         nameView.setText(filename);
         idView.setText(fileDetails.getId());
         sizeView.setText(size + " bytes");
         mimeTypeView.setText(mimeType);
         createdTimeView.setText(createdDateString);
+
+        if (mimeType.equals(DriveExplorer.folderMimeType)){
+            imageView.setBackgroundResource(R.drawable.ic_folder);
+        }
+        else if(mimeType.equals("image/jpeg") || mimeType.equals("image/png") || mimeType.equals("image/gif")){
+            imageView.setBackgroundResource(R.drawable.ic_image);
+        }
+        else if(mimeType.equals("application/pdf")){
+            imageView.setBackgroundResource(R.drawable.ic_pdf);
+        }
+        else {
+            imageView.setBackgroundResource(R.drawable.ic_file);
+        }
     }
 }
